5c5192f93b4879c9f1aa2a31a705b53857a7a05d
Artem <ignatev.artem.nik@gmail.com>
2021-04-17 23:48:24 +0400

    Переменные

---
+++ Переменные/script.js
@@ -0,0 +1,15 @@
+let message = "Hello world!";
+let helloMessage ='Hello';
+
+let $ ='Dollar';
+let _ = 'Underscore';
+
+let message2 = 'Hello world message 2';
+
+let сообщение = 'сообщение'
+
+console.log(message)
+console.log(helloMessage)
+console.log($)
+console.log(_)
+console.log(сообщение)
