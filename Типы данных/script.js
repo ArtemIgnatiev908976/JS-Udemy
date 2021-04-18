22c10ff865bec829e620dc0f4352942e6e2f9862
Artem <ignatev.artem.nik@gmail.com>
2021-04-18 12:58:53 +0400

    Типы данных

---
+++ Типы данных/script.js
@@ -0,0 +1,27 @@
+/*
+
+Number
+String
+Boolean
+Object
+Null
+Undefined
+
+ */
+
+let number = 42;
+let string = 'Message';
+let isTrue = true // false
+let object = {a:1};
+let isNull = null;
+let undef = undefined;
+
+
+
+
+console.log(typeof  number);
+console.log(typeof  string);
+console.log(typeof  isTrue);
+console.log(typeof  object);
+console.log(typeof  isNull);
+console.log(typeof  undef);
