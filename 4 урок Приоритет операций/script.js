eeb5fe7bb5af1ff1110ec02f0b40c90d8f4a8006
Artem <ignatev.artem.nik@gmail.com>
2021-04-18 13:52:04 +0400

    Приоритеты операций

---
+++ 4 урок Приоритет операций/script.js
@@ -0,0 +1,11 @@
+let result = 12 - 6 / 3;
+let result2 = 3 + 4 * 2;
+
+let isGreater = 20 - 6 * 3 >= 4;
+let isGreater2 = 20 - 6 * 3 >= 1;
+//             3   13  14   11   приоритеты
+
+console.log('12 -6 / 3:', result)  //10
+console.log('3+4*2:', result2)  //11
+console.log('20 - 6 * 3 >= 4:', isGreater)  //false
+console.log('20 - 6 * 3 >= 1:', isGreater2)  //true
