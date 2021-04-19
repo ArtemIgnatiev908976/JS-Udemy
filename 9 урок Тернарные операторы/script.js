42f587834d2c0e6a3288705505573b5e0e4e12d7
Artem <ignatev.artem.nik@gmail.com>
2021-04-19 17:03:27 +0400

    Тернарные операторы

---
+++ 9 урок Тернарные операторы/script.js
@@ -0,0 +1,28 @@
+
+//false
+// 0 null undefined '' NaN
+
+// let empty = ''
+//
+// if (NaN){
+//     console.log('Значение true')
+// }else{
+//     console.log('Значение false')
+// }
+
+// Тернарный оператор
+
+//4 ? console.log('Значение true') :  console.log('Значение false')
+
+
+let personAge = 20
+
+
+// if(personAge<18){
+//     message='Человек еще не совершенно летний'
+// }else {
+//     message = 'Человек совершенно летний'
+// }
+let message = personAge < 18 ? 'Человек еще не совершенно летний' : 'Человек совершенно летний'
+
+console.log(message)
