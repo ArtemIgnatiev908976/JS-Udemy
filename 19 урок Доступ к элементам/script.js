e7e920df295c1915e25989af62e3fbb069747ddd
Artem <ignatev.artem.nik@gmail.com>
2021-04-21 13:51:40 +0400

    Доступ к элементам

---
+++ 19 урок Доступ к элементам/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// let div = document.getElementById('playground')
+//
+// let p = document.getElementsByClassName('text')
+// let h1 = document.getElementsByTagName('h1')
+//
+// console.log(div)
+// console.log(p)
+// console.log(h1)
+
+// тот же самый результат новый метод
+
+let div = document.querySelector('#playground');
+let p = document.querySelectorAll('.text');
+let h1 = document.querySelector('h1');
+let ul = document.querySelector('div#playground ul')
+console.log(div)
+console.log(p)
+console.log(h1)
+console.log(ul)
