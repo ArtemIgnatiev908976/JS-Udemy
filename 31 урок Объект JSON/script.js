e8ef6ad07402e681be2105153b4c20e1e312c4c6
Artem <ignatev.artem.nik@gmail.com>
2021-04-27 12:50:03 +0400

    Объект JSON

---
+++ 31 урок Объект JSON/script.js
@@ -0,0 +1,14 @@
+let person ={
+    name: 'Max',
+    age: '26',
+    car: {
+        model: 'Ford'
+    },
+    job: 'Frontend',
+    friends: ['Elena', 'Igor']
+}
+
+let str = JSON.stringify(person)
+console.log(str)
+
+console.log(JSON.parse(str))
