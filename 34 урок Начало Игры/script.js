8d4fc0e9c8047488c3d41402214ddd8e12c663b5
Artem <ignatev.artem.nik@gmail.com>
2021-04-27 17:57:36 +0400

    1 урок
    Добавили событие на кнопку и замену бэкграунда

---
+++ 34 урок Начало Игры/script.js
@@ -0,0 +1,19 @@
+let $start = document.querySelector('#start')
+let $game = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+
+
+function startGame(){
+    $start.classList.add('hide')
+    $game.style.backgroundColor = '#fff'
+
+    renderBox()
+}
+
+
+function renderBox(){
+    
+}
+
+
