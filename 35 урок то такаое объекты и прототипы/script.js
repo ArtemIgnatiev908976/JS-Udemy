2dfc021f030a8d04819dcba330dbd73035604067
Artem <ignatev.artem.nik@gmail.com>
2021-04-28 18:01:41 +0400

    что такое объекты и прототипы

---
+++ 35 урок то такаое объекты и прототипы/script.js
@@ -0,0 +1,14 @@
+// number, string, boolean, null, undefined
+// object
+
+let car = {
+    name: 'Ford',
+    year:2015,
+    person:{
+
+    }
+}
+
+
+// car.__proto__ => object.prototype
+console.log(car)
