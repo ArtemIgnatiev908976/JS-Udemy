dcb4452912bf8c94f4a022aee3f28e2f64721a82
Artem <ignatev.artem.nik@gmail.com>
2021-04-28 14:25:47 +0400

    Вывод результата

--- 34 урок Начало Игры/script.js
+++ 34 урок Начало Игры/script.js
@@ -1,31 +1,47 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
 let $time = document.querySelector('#time')
+let $result = document.querySelector('#result')
 
 let score = 0
 let isGameStarted = false
+
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
+
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
 
+function setGameTime() {
+    let time = 5
+    $time.textContent = time.toFixed(1)
+}
+
+
 function startGame() {
+
+    score = 0
+    setGameTime()
+    $timeHeader.classList.remove('hide')
+    $resultHeader.classList.add('hide')
     isGameStarted = true
     $start.classList.add('hide')
     $game.style.backgroundColor = '#fff'
 
-    let  interval = setInterval(function (){
+    let interval = setInterval(function () {
 
-        let time =parseFloat($time.textContent)
+        let time = parseFloat($time.textContent)
         console.log('interval', $time.textContent)
 
-        if (time<=0){
+        if (time <= 0) {
             clearInterval(interval)
             endGame()
             //end game
 
-        }else{
-            $time.textContent = (time-0.1).toFixed(1)
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
         }
     }, 100)
 
@@ -33,12 +49,23 @@ function startGame() {
 }
 
 
-function endGame(){
-isGameStarted=false
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+
+function endGame() {
+    isGameStarted = false
+    setGameScore()
+    $start.classList.remove('hide')
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    $timeHeader.classList.add('hide')
+    $resultHeader.classList.remove('hide')
 }
 
 function handleBoxClick(event) {
-    if(!isGameStarted){
+    if (!isGameStarted) {
         return
     }
     if (event.target.dataset) {
@@ -54,17 +81,17 @@ function renderBox() {
     $game.innerHTML = ''
 
     let box = document.createElement('div')
-    let boxSize = getRandom(30,100)
+    let boxSize = getRandom(30, 100)
     let gameSize = $game.getBoundingClientRect()
     let maxTop = gameSize.height - boxSize
-    let maxLeft = gameSize.width-boxSize
+    let maxLeft = gameSize.width - boxSize
     console.log(gameSize)
 
-    box.style.height = box.style.width = boxSize+'px'
+    box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = getRandom(0,maxTop)+'px'
-    box.style.left = getRandom(0,maxLeft)+'px'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-box', 'true')
     $game.insertAdjacentElement('afterbegin', box)
