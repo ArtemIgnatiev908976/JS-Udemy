0748358038286b37a2949ba9c7677a86f25caefc
Artem <ignatev.artem.nik@gmail.com>
2021-05-05 14:39:47 +0400

    Переменные let const

---
+++ 43 урок переменные Let Const/script.js
@@ -0,0 +1,24 @@
+// let color = 'red';
+// color = 'blue'
+// console.log(color)
+
+
+
+// for (let i = 0; i<5; i++){
+//    setTimeout(function (){
+//        console.log(i)
+//    },2000)
+// }
+
+
+const HEX = '#FFAABB'
+
+const array = [1,2]
+const obj = { a:1}
+array.unshift(4)
+
+obj.b = 2
+console.log(array)
+console.log(obj)
+
+document.querySelector('h1').style.color=HEX
