e10a4997b9bb83031a1b8ea3dfb68b20a66c1c75
Artem <ignatev.artem.nik@gmail.com>
2021-05-07 12:44:06 +0400

    Строки, шаблонизация

---
+++ 48 урок Строки/script.js
@@ -0,0 +1,25 @@
+/// `` - шаблонизация внутри можно использоваться ${}
+
+const createLink = ({path,name}) =>{
+return `<a target="_blank" href="${path}">${name}</a>`
+}
+
+const ul =  document.querySelector('ul')
+
+const google = `<li> ${createLink({path: 'https://google.com', name: 'Google'})}</li>`
+
+const yandex = `<li> ${createLink({path: 'https://ya.ru', name: 'Yandex'})}</li>`
+
+
+ul.insertAdjacentHTML('afterbegin', google )
+ul.insertAdjacentHTML('afterbegin', yandex)
+
+
+
+const strToLog = `
+Hello
+World   ada
+asdda
+
+`
+console.log(strToLog)
